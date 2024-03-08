5591f65adfd5432a6857d49009eef1154d119dc5
Francesco <francesco.bellentani@gmail.com>
2024-03-08 17:15:43 +0100

    add contact-manager extension

---
+++ development/ext/contact-manager/README.md
@@ -0,0 +1,32 @@
+# contact-manager extension
+[Example Contact Manager Behaviour extensions from Community](https://community.backbase.com/documentation/DBS/latest/contacts_extend_behavior)
+
+Build with JDK 17.
+
+## Description
+This example shows you how to modify the default behavior of the Contacts presentation service. 
+This modification checks for specific country values and raises a flag if, for example, a country is on a blacklist.
+
+## How to use
+To use your service extension, you have to include the JAR build from this project to the CLASSPATH of the service.
+
+[When you run a service as a bootable jar](https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#executable-jar-property-launcher-features), 
+you can use the `loader.path` command line argument to add JARs to the CLASSPATH. 
+`loader.path` takes a comma-separated list of locations, which can reference JARs or 
+directories containing one or more JARs. For example: 
+
+    java -Dloader.path=/lib,/path/to/my.jar -jar myservice-boot.jar
+    
+If you are not running the Service as a war, use the mechanism available in your application server.
+
+### Docker
+By adding the profile `docker-image` to the build command, the project builds and pushes a new docker image
+that already contains the extension:
+
+
+    mvn clean package -Pdocker-image
+
+Add the `local-client` profile to skip the image push to the remote registry.
+
+
+To use the extension in other scenarios, please check the community documentation.
---
+++ development/ext/contact-manager/src/main/java/com/backbase/dbs/contactmanager/extension/advanced/CustomEndpoints.java
@@ -0,0 +1,36 @@
+
+package com.backbase.dbs.contactmanager.extension.advanced;
+
+import com.backbase.buildingblocks.presentation.errors.BadRequestException;
+import com.backbase.dbs.contactmanager.contact.dto.Contact;
+import com.backbase.dbs.contactmanager.contact.dto.InternalRequestWrapper;
+import java.util.List;
+import org.apache.camel.Consume;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * The custom camel Endpoints to be used in the behavior extension.
+ */
+@Component
+public class CustomEndpoints {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomEndpoints.class);
+    private static final List<String> HIGH_RISK_COUNTRIES = List.of("NL");
+    public static final String VALIDATION_INTERCEPTOR = "direct:contact.validate.interceptor";
+
+    @Consume(VALIDATION_INTERCEPTOR)
+    public void executeValidation(InternalRequestWrapper<Contact> internalRequest) {
+
+        LOGGER.info("======== In custom endpoint for validation for user " + internalRequest.getBbId() + " ========");
+        Contact contact = internalRequest.getInternalRequest().getData();
+        if ("sara".equals(internalRequest.getBbId()) && contact.getCountry() != null && isHighRiskCountry(contact.getCountry())) {
+            throw new BadRequestException("User is not allowed to handle a counterparty from a high risk country");
+        }
+    }
+
+    private boolean isHighRiskCountry(String countryCode) {
+        return HIGH_RISK_COUNTRIES.contains(countryCode);
+    }
+}
