1a347ab5a407526918d75db69cb12b2fc37436bf
Tarun Bharti <tarun@backbase.com>
2024-04-19 14:18:42 +0200

    Removed password from aws script

--- development/config/aws/scripts/danske-poc-env-db-cleanup.sh
+++ development/config/aws/scripts/danske-poc-env-db-cleanup.sh
@@ -1,19 +1,19 @@
 # Dropping existing databases
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "DROP DATABASE IF EXISTS accesscontrol_pandp;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "DROP DATABASE IF EXISTS arrangement_manager;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "DROP DATABASE IF EXISTS backbase_identity;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "DROP DATABASE IF EXISTS retail_onboarding;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "DROP DATABASE IF EXISTS user_manager;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "DROP DATABASE IF EXISTS accesscontrol_pandp;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "DROP DATABASE IF EXISTS arrangement_manager;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "DROP DATABASE IF EXISTS backbase_identity;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "DROP DATABASE IF EXISTS retail_onboarding;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "DROP DATABASE IF EXISTS user_manager;" &
 
 # Wait for database deletion operations to finish
 wait
 
 # Creating new databases
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "CREATE DATABASE accesscontrol_pandp;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "CREATE DATABASE arrangement_manager;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "CREATE DATABASE backbase_identity;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "CREATE DATABASE retail_onboarding CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" &
-mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -pmysqldb2024 -e "CREATE DATABASE user_manager;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "CREATE DATABASE accesscontrol_pandp;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "CREATE DATABASE arrangement_manager;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "CREATE DATABASE backbase_identity;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "CREATE DATABASE retail_onboarding CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" &
+mysql -h danske-poc-db-instance-1.cjgq66g4s8nw.us-east-1.rds.amazonaws.com -u admin -p -e "CREATE DATABASE user_manager;" &
 
 # Wait for database creation operations to finish
 wait
