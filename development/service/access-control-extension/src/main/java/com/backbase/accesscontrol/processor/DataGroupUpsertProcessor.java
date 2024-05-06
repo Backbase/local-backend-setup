3665b605c441620d8e83b1c495e2c8ff82dc4658
Tarun Bharti <tarun@backbase.com>
2024-05-06 22:13:38 +0200

    New changes to support other data group types other than arrangements

--- development/service/access-control-extension/src/main/java/com/backbase/accesscontrol/processor/DataGroupUpsertProcessor.java
+++ development/service/access-control-extension/src/main/java/com/backbase/accesscontrol/processor/DataGroupUpsertProcessor.java
@@ -2,11 +2,15 @@ package com.backbase.accesscontrol.processor;
 
 import com.backbase.accesscontrol.domain.dto.DataGroupBaseDto;
 import com.backbase.accesscontrol.domain.dto.PresentationDataGroupUpdateDto;
+import com.backbase.accesscontrol.domain.dto.PresentationItemIdentifierDto;
 import com.backbase.accesscontrol.domain.dto.PresentationServiceAgreementWithDataGroupsDto;
 import com.backbase.accesscontrol.mapper.PutDataGroupsEventMapper;
 import com.backbase.accesscontrol.service.facades.v3.DataGroupServiceFacade;
-import com.backbase.accesscontrol.util.DataItemsUtil;
+import com.backbase.accesscontrol.util.strategy.ContactItemStrategy;
+import com.backbase.accesscontrol.util.strategy.DataItemExternalIdConverter;
+import com.backbase.dbs.accesscontrol.api.client.v3.ServiceAgreementsApi;
 import com.backbase.integration.accessgroup.rest.spec.v3.IntegrationDataGroupItemBatchPutRequestBody;
+import com.backbase.integration.accessgroup.rest.spec.v3.IntegrationItemIdentifier;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,11 +21,13 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class DataGroupUpsertProcessor {
 
-    private final DataItemsUtil dataItemsUtil;
     private final DataGroupServiceFacade dataGroupServiceFacade;
     private final PutDataGroupsEventMapper mapper;
+    private final ServiceAgreementsApi serviceAgreementsApi;
+    private final DataItemExternalIdConverter dataItemExternalIdConverter;
 
-    public IntegrationDataGroupItemBatchPutRequestBody process(IntegrationDataGroupItemBatchPutRequestBody requestPayload) {
+    public IntegrationDataGroupItemBatchPutRequestBody process(
+        IntegrationDataGroupItemBatchPutRequestBody requestPayload) {
         var searchDataGroupRequest = mapper.mapToPresentationSearchDataGroupsRequestDto(requestPayload);
         var searchResult = dataGroupServiceFacade.searchDataGroups(searchDataGroupRequest,
             requestPayload.getType());
@@ -29,14 +35,14 @@ public class DataGroupUpsertProcessor {
         if (isDataGroupExist(searchResult, requestPayload.getName())) {
             log.info("Data group found: {}", searchResult.get(0).getDataGroups().get(0).getName());
             PresentationDataGroupUpdateDto updateDto = mapper.mapToDataGroupUpdateDto(requestPayload);
-            dataItemsUtil.updateDataItems(requestPayload.getDataItems(), updateDto);
+            convertDataItemExternalIdsToInternal(requestPayload, updateDto);
             log.info("Updating Data Group: {}", updateDto);
             dataGroupServiceFacade.updateDataGroup(updateDto);
         } else {
 
             log.info("Data group not found: {}", searchResult.get(0).getDataGroups().get(0).getName());
             DataGroupBaseDto createDto = mapper.mapToDataGroupBaseDto(requestPayload);
-            dataItemsUtil.updateDataItems(requestPayload.getDataItems(), createDto);
+            convertDataItemExternalIdsToInternal(requestPayload, createDto);
             log.info("Creating Data Group: {}", createDto);
             dataGroupServiceFacade.createDataGroup(createDto);
         }
@@ -52,5 +58,48 @@ public class DataGroupUpsertProcessor {
             .flatMap(List::stream)
             .anyMatch(dataGroup -> dataGroup.getName().equals(name));
     }
+
+    private void convertDataItemExternalIdsToInternal(IntegrationDataGroupItemBatchPutRequestBody requestBody,
+        PresentationDataGroupUpdateDto dto) {
+        if (containsExternalItemIdentifier(requestBody.getDataItems())) {
+
+            String serviceAgreementInternalId = ContactItemStrategy.DATA_ITEM_TYPE.equals(requestBody.getType())
+                ? serviceAgreementsApi.getServiceAgreementExternalId(
+                    requestBody.getDataGroupIdentifier().getNameIdentifier().getExternalServiceAgreementId())
+                .getId()
+                : null;
+            List<String> internalIds = dataItemExternalIdConverter
+                .convertDataItemExternalIdsToInternal(requestBody.getDataItems(),
+                    requestBody.getType(), serviceAgreementInternalId);
+            dto.setDataItems(internalIds.stream().map(id -> {
+                    var itemDto = new PresentationItemIdentifierDto();
+                    itemDto.setInternalIdIdentifier(id);
+                    return itemDto;
+                })
+                .toList());
+        }
+    }
+
+    private boolean containsExternalItemIdentifier(List<IntegrationItemIdentifier> list) {
+        return list != null && list.stream()
+            .anyMatch(dataItem -> dataItem != null && dataItem.getExternalIdIdentifier() != null);
+    }
+
+    private void convertDataItemExternalIdsToInternal(IntegrationDataGroupItemBatchPutRequestBody requestBody,
+        DataGroupBaseDto dto) {
+        if (containsExternalItemIdentifier(requestBody.getDataItems())) {
+
+            String serviceAgreementInternalId = ContactItemStrategy.DATA_ITEM_TYPE.equals(requestBody.getType())
+                ? serviceAgreementsApi.getServiceAgreementExternalId(
+                    requestBody.getDataGroupIdentifier().getNameIdentifier().getExternalServiceAgreementId())
+                .getId()
+                : null;
+            List<String> internalIds = dataItemExternalIdConverter
+                .convertDataItemExternalIdsToInternal(requestBody.getDataItems(),
+                    requestBody.getType(), serviceAgreementInternalId);
+            dto.setItems(internalIds);
+        }
+    }
+
 
 }
---
+++ development/service/access-control-extension/src/main/java/com/backbase/accesscontrol/util/strategy/DataItemExternalIdConverter.java
@@ -0,0 +1,14 @@
+package com.backbase.accesscontrol.util.strategy;
+
+import com.backbase.integration.accessgroup.rest.spec.v3.IntegrationItemIdentifier;
+import java.util.List;
+
+public interface DataItemExternalIdConverter {
+
+    List<String> convertDataItemExternalIdsToInternal(
+        List<IntegrationItemIdentifier> dataItems, String type, String serviceAgreementInternalId);
+
+    String getInternalId(String externalId, String type, String serviceAgreementInternalId);
+
+    boolean converterExistsByDataItemType(String dataItemType);
+}
