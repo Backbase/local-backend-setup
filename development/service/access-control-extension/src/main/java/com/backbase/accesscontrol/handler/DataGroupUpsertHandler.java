1aa2aeed0c9e4328204e87f4373941638fa28261
Tarun Bharti <tarun@backbase.com>
2024-05-06 23:34:47 +0200

    Updated code to send output to same partition as input channel

--- development/service/access-control-extension/src/main/java/com/backbase/accesscontrol/handler/DataGroupUpsertHandler.java
+++ development/service/access-control-extension/src/main/java/com/backbase/accesscontrol/handler/DataGroupUpsertHandler.java
@@ -4,13 +4,15 @@ import com.backbase.accesscontrol.processor.DataGroupUpsertProcessor;
 import com.backbase.integration.accessgroup.rest.spec.v3.IntegrationDataGroupItemBatchPutRequestBody;
 import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component("upsertDataGroup")
 @Slf4j
 public class DataGroupUpsertHandler implements
-    Function<Message<IntegrationDataGroupItemBatchPutRequestBody>, IntegrationDataGroupItemBatchPutRequestBody> {
+    Function<Message<IntegrationDataGroupItemBatchPutRequestBody>, Message<IntegrationDataGroupItemBatchPutRequestBody>> {
 
     private final DataGroupUpsertProcessor dataGroupUpsertProcessor;
 
@@ -19,11 +21,14 @@ public class DataGroupUpsertHandler implements
     }
 
     @Override
-    public IntegrationDataGroupItemBatchPutRequestBody apply(
+    public Message<IntegrationDataGroupItemBatchPutRequestBody> apply(
         Message<IntegrationDataGroupItemBatchPutRequestBody> message) {
         log.info("Upsert Data Message received: {}", message);
+        var partitionId = message.getHeaders().get("kafka_receivedPartitionId", Integer.class);
 
         IntegrationDataGroupItemBatchPutRequestBody requestPayload = message.getPayload();
-        return dataGroupUpsertProcessor.process(requestPayload);
+        return MessageBuilder.withPayload(dataGroupUpsertProcessor.process(requestPayload))
+            .setHeader(KafkaHeaders.PARTITION, partitionId)
+            .build();
     }
 }
