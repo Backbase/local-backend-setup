4d8aa00813211c5da788538c3311f36c24776140
Tarun Bharti <tarun@backbase.com>
2024-10-22 23:44:02 +0200

    Added tracing config

---
+++ development/docker-compose/scripts/reset_local_setup.sh
@@ -0,0 +1,12 @@
+# This command stops and removes the containers and volumes defined in the docker-compose.yml file,
+# including MySQL and its data, and then deletes the Docker network.
+docker compose down -v
+
+docker rm $(docker ps -a -q)
+
+colima stop
+
+colima start --cpu 4 --memory 16 --disk 100
+
+# start the service with bootstrap profile
+docker compose --profile=bootstrap up -d --remove-orphans
