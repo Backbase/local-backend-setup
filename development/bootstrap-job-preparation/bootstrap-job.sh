638def7666532149d5e759e48f5b83d44f298248
mostafarastgar@gmail.com <mostafar@backbase.com>
2024-12-03 16:53:24 +0100

    add data

--- development/bootstrap-job-preparation/bootstrap-job.sh
+++ development/bootstrap-job-preparation/bootstrap-job.sh
@@ -14,15 +14,6 @@ cd target/bootstrap-job
 
 mvn clean install -s ../../settings.xml
 
-docker run -d -p 8000:5000 --name local-registry registry:2
-
-#curl -X GET http://localhost:8000/v2/_catalog
-
-mvn -ntp -B package -s ../../settings.xml -pl :bootstrap-job -Pdocker-image -Pno-latest-tag -Dskip.unit.tests=true \
-          -Dskip.integration.tests=true -DskipDocker -Dimage=127.0.0.1:8000/bootstrap-job \
-          -Djib.to.tags="${BOOTSTRAP_JOB_VERSION}" -Djib.allowInsecureRegistries=true
-
-docker pull localhost:8000/bootstrap-job:${BOOTSTRAP_JOB_VERSION}
-
-docker stop local-registry
-docker remove local-registry
+mvn -ntp -B package -s ../../settings.xml -pl :bootstrap-job -Pdocker-image,local-client,no-latest-tag \
+          -Dskip.unit.tests=true -Dskip.integration.tests=true  -Dimage=bootstrap-job \
+          -Djib.to.tags="${BOOTSTRAP_JOB_VERSION}"
