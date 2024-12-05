d9a66f9a3b2237ad9de25ca825c5d7002bd53591
mostafarastgar@gmail.com <mostafar@backbase.com>
2024-12-05 12:05:27 +0100

    BEFOUND-1640-replace-stream-with-bootstrap-job

--- development/bootstrap-job-preparation/bootstrap-job.sh
+++ development/bootstrap-job-preparation/bootstrap-job.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
-if [ $# -ne 2 ]; then
-  echo "Usage: $0 <repo_username> <repo_api_key>"
-  exit 1
-fi
-source ../docker-compose/.env
-export ARTIFACTS_CREDS_USR=$1
-export ARTIFACTS_CREDS_PSW=$2
+set -e  # Exit immediately if a command exits with a non-zero status
 
+# Ensure credentials are unset even if the script exits prematurely
+trap 'unset ARTIFACTS_CREDS_USR; unset ARTIFACTS_CREDS_PSW' EXIT
+
+# Load environment variables
+source ../docker-compose/.env
+
+# Prompt for Artifactory credentials
+read -p "Enter your Artifactory 'Username' (you can take the 'User Profile' from https://repo.backbase.com/ui/user_profile): " artifacts_creds_usr
+export ARTIFACTS_CREDS_USR="$artifacts_creds_usr"
+
+read -sp "Enter your 'API Key' (you can take it from https://repo.backbase.com/ui/user_profile): " artifacts_creds_psw
+export ARTIFACTS_CREDS_PSW="$artifacts_creds_psw"
+echo
+
+echo "Downloading Bootstrap job ..."
 mvn clean process-resources -Dbootstrap.job.version="$BOOTSTRAP_JOB_VERSION" -s settings.xml
+echo "Downloading Bootstrap job succeeded."
 
 cd target/bootstrap-job
-
+echo "Building Bootstrap job ..."
 mvn clean install -s ../../settings.xml
+echo "Building Bootstrap job succeeded."
 
+echo "Building Bootstrap job docker image ..."
 mvn -ntp -B package -s ../../settings.xml -pl :bootstrap-job -Pdocker-image,local-client,no-latest-tag \
-          -Dskip.unit.tests=true -Dskip.integration.tests=true  -Dimage=bootstrap-job \
-          -Djib.to.tags="$BOOTSTRAP_JOB_VERSION"
+    -Dskip.unit.tests=true -Dskip.integration.tests=true -Dimage=bootstrap-job \
+    -Djib.to.tags="$BOOTSTRAP_JOB_VERSION"
+echo "Building Bootstrap job docker image succeeded."
+
+echo "Bootstrap job preparation completed successfully!"
