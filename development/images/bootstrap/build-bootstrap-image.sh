44703038160bed30af96e66d15dcf3db5c845bda
mostafa-rastegar-awx <mostafa.rastegar@airwallex.com>
2024-12-09 12:54:44 +0100

    make it windows compatible

--- development/images/bootstrap/build-bootstrap-image.sh
+++ development/images/bootstrap/build-bootstrap-image.sh
@@ -5,6 +5,16 @@ set -e  # Exit immediately if a command exits with a non-zero status
 source ../../docker-compose/.env
 
 cd target/bootstrap-job
+PLUGIN_EXCLUSION="<build>\n    <plugins>\n        <plugin>\n            <groupId>org.codehaus.gmavenplus</groupId>\n            <artifactId>gmavenplus-plugin</artifactId>\n            <executions>\n                <execution>\n                    <id>starter</id>\n                    <phase>none</phase> <!-- Disable execution entirely -->\n                </execution>\n            </executions>\n        </plugin>\n    </plugins>\n</build>"
+
+awk '
+/<\/project>/ {
+    print plugin_exclusion
+    print $0
+    next
+}
+{ print $0 }
+' plugin_exclusion="$PLUGIN_EXCLUSION" pom.xml > pom.xml.tmp && mv pom.xml.tmp pom.xml
 echo "Building Bootstrap job ..."
 mvn clean install
 echo "Building Bootstrap job succeeded."
